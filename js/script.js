afe2ad6758b57786a2fe08d31cf0bd938bb7a9f1
NathanChem <chemama.nathan2004@gmail.com>
2025-11-26 13:44:20 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const mot = "coucou";
+
+let nomUtilisateur = prompt(`Entrez ${mot} :`);
+
+if (nomUtilisateur == mot) {
+  window.alert("GG Mec");
+} else {
+  window.alert("T'es trop guez mec");
+}
