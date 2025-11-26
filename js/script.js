ff8a521e92cff1475c38ae66908b2d215273b42b
NathanChem <chemama.nathan2004@gmail.com>
2025-11-26 14:01:36 +0100

    RESTART

--- js/script.js
+++
@@ -1,9 +0,0 @@
-const mot = "coucou";
-
-let nomUtilisateur = prompt(`Entrez ${mot} :`);
-
-if (nomUtilisateur == mot) {
-  window.alert("GG Mec");
-} else {
-  window.alert("T'es trop guez mec");
-}
